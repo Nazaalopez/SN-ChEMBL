e2ab31e9aeb4770dd6f28eeaebce8b9d8316256d
Nazareth Lopez <58481904+Nazaalopez@users.noreply.github.com>
2020-09-02 17:19:44 -0600

    Create views.py

---
+++ SN_ChEMBL/views.py
@@ -0,0 +1,17 @@
+  
+from model import resolve
+from controller import render_entity
+from bottle import request
+from chembl_slack import app
+
+@app.post('/chem')
+def chem():
+    text = request.forms['text']  
+    ret = resolve(text)
+    if not ret:
+        return "Provided identifier couldn't be resolved :white_frowning_face:"
+    if isinstance(ret, basestring):
+        return ret
+    entity_type, entity_id = ret
+    msg = render_entity(entity_type, entity_id)
+    return msg
