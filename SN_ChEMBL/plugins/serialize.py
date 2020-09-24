be37893bdf6b9140d3d543737fef6a66ce6d4d03
Nazareth Lopez <58481904+Nazaalopez@users.noreply.github.com>
2020-09-24 14:52:14 -0600

    Create serialize.py

---
+++ SN_ChEMBL/plugins/serialize.py
@@ -0,0 +1,20 @@
+
+from bottle import request, response
+from SN_ChEMBL import config
+import json
+
+class Serialize(object):
+    name = 'serialize'
+    api = 2
+
+    def apply(self, fn, context):
+        def _serialize(*args, **kwargs):
+            body = fn(*args, **kwargs)
+            if isinstance(body, basestring):
+                return body
+            elif isinstance(body, dict):
+                response.content_type = 'application/json'
+                body["response_type"] = config.get('response_type', 'ephemeral')
+                return json.dumps(body)
+            return None               
+        return _serialize
